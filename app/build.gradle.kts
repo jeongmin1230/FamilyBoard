38b1ac5a4b65e20b2bc866bb1572d1756ac3ebb4
jeongmin <tmxk0810@gmail.com>
2024-01-11 16:38:30 +0900

    <Announcement.kt, MyInformation.kt, QA.kt> - 사용 방법 composable 추가
    <AnnouncementViewModel.kt, MyInformationViewModel.kt, QAViewModel.kt>
    - 데이터 베이스 주소 object 로 한 파일에 관리 -> 뷰 모델에 하나하나 써 놨던 거 수정
    
    <CustomComosable.kt>
    - q_a 화면에서 길게 눌러야 답변 달 수 있던 거 한 번만 클릭 해도 답변 다는 화면으로 넘어가게 수정
    
    <FamilyInformationResponse.kt, FamilyInformationViewModel.kt>
    - 가족 정보 가져올 때 이메일은 필요하지 않아서 삭제
    
    <FirebaseAllPath.kt>
    - 데이터 베이스에서 필요한 경로 싱글톤으로 정리(더 간결, 편함)
    
    <strings.xml>
    - 일부 string 수정
    
    2024. 1. 11
    versionCode 7 & versionName 2.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "com.jm.familyboard"
         minSdk = 24
         targetSdk = 34
-        versionCode = 6
+        versionCode = 7
         versionName = "2.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
